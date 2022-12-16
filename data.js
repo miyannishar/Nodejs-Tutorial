f6d265c4fb324a5050da21c1f4e2522d0d7625a6
miyannishar <miyannishar786@gmail.com>
2022-12-16 16:13:00 -0500

    Creating my first api by taking data from another

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+    {name: "Nishar Miya", email:"nisharmiya@gmail.com"},
+    {name: "Ahamad Mamun", email:"ahamadmamun@gmail.com"},
+    {name: "Ahamad Nishar", email:"ahamadnishar@gmail.com"},
+    {name: "Miyan Nishar", email:"miyannishar786@gmail.com"},
+];
+module.exports=data;
